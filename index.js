3e3418ab123ed110c4e1493a6cf8a410a8302770
Jameel Ahmed <jameel.webdev@gmail.com>
2024-04-16 05:55:51 +0000

    package

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const bodyParser = require("body-parser");
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/public", express.static(`${process.cwd()}/public`));
 
 app.get("/", function (req, res) {
